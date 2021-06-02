949d1dca484e03c6850baaa41e31fc99301e37d4
Kouki Badr <badreddinekouki1998@gmail.com>
2021-06-02 06:10:59 +0100

    initial commit

---
+++ app/src/main/java/com/bkapp/phonebook/ContactActivity.kt
@@ -0,0 +1,11 @@
+package com.bkapp.phonebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts)
+    }
+}
